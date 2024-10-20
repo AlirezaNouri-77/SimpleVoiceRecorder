a616c05c57007296d05cc012f7195718c7583f10
Alireza Nouri <71293106+AlirezaNouri-77@users.noreply.github.com>
2024-10-20 19:30:19 +0330

    impl recordRepository Test

---
+++ app/src/androidTest/java/com/shermanrex/recorderApp/data/repository/RecordRepositoryTest.kt
@@ -0,0 +1,107 @@
+package com.shermanrex.recorderApp.data.repository
+
+import android.net.Uri
+import androidx.documentfile.provider.DocumentFile
+import com.google.common.truth.Truth.assertThat
+import com.shermanrex.recorderApp.domain.model.Failure
+import com.shermanrex.recorderApp.domain.model.RecordModel
+import com.shermanrex.recorderApp.domain.model.RepositoryResult
+import com.shermanrex.recorderApp.domain.model.RootFailure
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class RecordRepositoryTest {
+
+  @MockK
+  private lateinit var recordRepository: RecordRepository
+
+  @Before
+  fun setup() {
+    recordRepository = mockk<RecordRepository>()
+  }
+
+  @Test
+  fun getRecords_return_loading() = runTest {
+    val documentFile = mockk<DocumentFile>()
+    coEvery { recordRepository.getRecords(any()) } returns flow {
+      emit(RepositoryResult.Loading)
+    }
+
+    val test = recordRepository.getRecords(documentFile).first()
+
+    assertThat(test).isEqualTo(RepositoryResult.Loading)
+  }
+
+  @Test
+  fun getRecords_return_success_list() = runTest {
+    val resultList = mutableListOf<RepositoryResult<RecordModel, Failure>>()
+    val documentFile = mockk<DocumentFile>()
+    coEvery { recordRepository.getRecords(any()) } returns flow {
+      emit(RepositoryResult.Loading)
+      emit(RepositoryResult.Success(listOf()))
+    }
+
+    recordRepository.getRecords(documentFile).collect { resultList.add(it) }
+
+    assertThat(resultList).containsExactly(
+      RepositoryResult.Loading,
+      RepositoryResult.Success<RecordModel>(listOf())
+    )
+  }
+
+  @Test
+  fun getRecords_return_fail_empty() = runTest {
+    val resultList = mutableListOf<RepositoryResult<RecordModel, Failure>>()
+    val documentFile = mockk<DocumentFile>()
+    coEvery { recordRepository.getRecords(any()) } returns flow {
+      emit(RepositoryResult.Loading)
+      emit(RepositoryResult.Failure(Failure.Empty))
+    }
+
+    recordRepository.getRecords(documentFile).collect { resultList.add(it) }
+
+    assertThat(resultList).containsExactly(
+      RepositoryResult.Loading,
+      RepositoryResult.Failure<RootFailure>(Failure.Empty)
+    )
+  }
+
+  @Test
+  fun getRecord_by_single_uri_return_recordModel_if_uri_valid() = runTest {
+
+    val record = mockk<RecordModel>()
+
+    coEvery { recordRepository.getRecordByUri(any()).await() } returns record
+    val test = recordRepository.getRecordByUri(Uri.EMPTY).await()
+
+    assertThat(test).isEqualTo(record)
+  }
+
+  @Test
+  fun getRecord_by_single_uri_return_null_if_uri_not_valid() = runTest {
+
+    coEvery { recordRepository.getRecordByUri(any()).await() } returns null
+    val test = recordRepository.getRecordByUri(Uri.EMPTY).await()
+
+    assertThat(test).isNull()
+  }
+
+  @Test
+  fun getRecord_by_single_uri_return_deferred() = runTest {
+
+    val deferred = CompletableDeferred<RecordModel>()
+
+    coEvery { recordRepository.getRecordByUri(any()) } returns deferred
+    val test = recordRepository.getRecordByUri(Uri.EMPTY)
+
+    assertThat(test).isEqualTo(deferred)
+  }
+
+}
