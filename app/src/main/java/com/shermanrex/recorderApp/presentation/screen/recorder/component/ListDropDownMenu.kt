ba200b192a9fb58d8a9bc2a341d76566a1e17d43
unknown <shermanrex98@gmail.com>
2024-11-15 18:35:33 +0330

    apply changes in dropdownmenu due to api change

--- app/src/main/java/com/shermanrex/recorderApp/presentation/screen/recorder/component/ListDropDownMenu.kt
+++ app/src/main/java/com/shermanrex/recorderApp/presentation/screen/recorder/component/ListDropDownMenu.kt
@@ -2,6 +2,7 @@ package com.shermanrex.recorderApp.presentation.screen.recorder.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -16,7 +17,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.shermanrex.presentation.screen.component.util.NoRipple
 import com.shermanrex.recorderApp.R
 import com.shermanrex.recorderApp.domain.model.ui.DropDownMenuStateUi
 
@@ -30,67 +30,63 @@ fun ListDropDownMenu(
   onDeleteClick: () -> Unit,
   onSelectMode: () -> Unit,
 ) {
-  MaterialTheme(shapes = MaterialTheme.shapes.copy(RoundedCornerShape(20.dp))) {
-    DropdownMenu(
-      modifier = modifier
-        .background(MaterialTheme.colorScheme.primary)
-        .wrapContentSize(),
-      expanded = dropDownMenuState().showDropDown && !isSelectedMode,
-      onDismissRequest = { onDismiss() },
-      offset = dropDownMenuState().longPressOffset,
-    ) {
-      DropdownMenuItem(
-        text = { Text(text = "Delete", fontSize = 14.sp, fontWeight = FontWeight.Medium) },
-        leadingIcon = {
-          Icon(
-            modifier = Modifier.size(16.dp),
-            painter = painterResource(id = R.drawable.icon_delete),
-            contentDescription = "",
-            tint = MaterialTheme.colorScheme.onPrimary,
-          )
-        },
-        onClick = {
-          onDismiss()
-          onDeleteClick()
-        },
-        contentPadding = PaddingValues(horizontal = 7.dp, vertical = 4.dp),
-        interactionSource = NoRipple,
-      )
-      DropdownMenuItem(
-        text = { Text(text = "Rename", fontSize = 14.sp, fontWeight = FontWeight.Medium) },
-        leadingIcon = {
-          Icon(
-            modifier = Modifier.size(16.dp),
-            painter = painterResource(id = R.drawable.icon_rename),
-            contentDescription = "",
-            tint = MaterialTheme.colorScheme.onPrimary,
-          )
-        },
-        contentPadding = PaddingValues(horizontal = 7.dp, vertical = 4.dp),
-        onClick = {
-          onDismiss()
-          onRenameClick()
-        },
-        interactionSource = NoRipple,
-      )
-      DropdownMenuItem(
-        text = { Text(text = "Select", fontSize = 14.sp, fontWeight = FontWeight.Medium) },
-        leadingIcon = {
-          Icon(
-            modifier = Modifier.size(16.dp),
-            painter = painterResource(id = R.drawable.icon_rename),
-            contentDescription = "",
-            tint = MaterialTheme.colorScheme.onPrimary,
-          )
-        },
-        contentPadding = PaddingValues(horizontal = 7.dp, vertical = 4.dp),
-        onClick = {
-          onDismiss()
-          onSelectMode()
-        },
-        interactionSource = NoRipple,
-      )
-    }
+  DropdownMenu(
+    modifier = modifier
+      .background(MaterialTheme.colorScheme.primary)
+      .wrapContentSize()
+      .padding(4.dp),
+    shape = RoundedCornerShape(15.dp),
+    expanded = dropDownMenuState().showDropDown && !isSelectedMode,
+    onDismissRequest = { onDismiss() },
+    offset = dropDownMenuState().longPressOffset,
+  ) {
+    DropdownMenuItem(
+      text = { Text(text = "Delete", fontSize = 15.sp, fontWeight = FontWeight.Medium) },
+      leadingIcon = {
+        Icon(
+          modifier = Modifier.size(18.dp),
+          painter = painterResource(id = R.drawable.icon_delete),
+          contentDescription = "",
+          tint = MaterialTheme.colorScheme.onPrimary,
+        )
+      },
+      onClick = {
+        onDismiss()
+        onDeleteClick()
+      },
+      contentPadding = PaddingValues(horizontal = 7.dp, vertical = 4.dp),
+    )
+    DropdownMenuItem(
+      text = { Text(text = "Rename", fontSize = 15.sp, fontWeight = FontWeight.Medium) },
+      leadingIcon = {
+        Icon(
+          modifier = Modifier.size(18.dp),
+          painter = painterResource(id = R.drawable.icon_rename),
+          contentDescription = "",
+          tint = MaterialTheme.colorScheme.onPrimary,
+        )
+      },
+      contentPadding = PaddingValues(horizontal = 7.dp, vertical = 4.dp),
+      onClick = {
+        onDismiss()
+        onRenameClick()
+      },
+    )
+    DropdownMenuItem(
+      text = { Text(text = "Select", fontSize = 15.sp, fontWeight = FontWeight.Medium) },
+      leadingIcon = {
+        Icon(
+          modifier = Modifier.size(18.dp),
+          painter = painterResource(id = R.drawable.icon_select_all_on),
+          contentDescription = "",
+          tint = MaterialTheme.colorScheme.onPrimary,
+        )
+      },
+      contentPadding = PaddingValues(horizontal = 7.dp, vertical = 4.dp),
+      onClick = {
+        onDismiss()
+        onSelectMode()
+      },
+    )
   }
-
 }
